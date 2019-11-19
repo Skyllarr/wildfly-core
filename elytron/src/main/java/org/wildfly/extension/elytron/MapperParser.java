1ddc41044533ce112688a80e634385cad16bccf4
dvilkola <dvilkola@redhat.com>
2019-11-19 15:29:35 +0100

    [WFCORE-4750] Using regex for role in Elytron

--- elytron/src/main/java/org/wildfly/extension/elytron/MapperParser.java
+++ elytron/src/main/java/org/wildfly/extension/elytron/MapperParser.java
@@ -205,6 +205,13 @@ class MapperParser {
             })
             .build();
 
+    private PersistentResourceXMLDescription regexRoleMapperParser = PersistentResourceXMLDescription.builder(RoleMapperDefinitions.getRegexRoleMapperDefinition().getPathElement())
+            .addAttribute(RoleMapperDefinitions.PATTERN)
+            .addAttribute(RoleMapperDefinitions.REPLACEMENT)
+            .addAttribute(RoleMapperDefinitions.KEEP_NON_MAPPED)
+            .addAttribute(RoleMapperDefinitions.REPLACE_ALL)
+            .build();
+
     private PersistentResourceXMLDescription addPrefixRoleMapperParser = PersistentResourceXMLDescription.builder(RoleMapperDefinitions.getAddPrefixRoleMapperDefinition().getPathElement())
             .addAttribute(RoleMapperDefinitions.PREFIX)
             .build();
@@ -380,6 +387,7 @@ class MapperParser {
                 .addChild(x509SubjectAltNameEvidenceDecoder) // new
                 .addChild(getCustomComponentParser(CUSTOM_EVIDENCE_DECODER)) // new
                 .addChild(aggregateEvidenceDecoderParser) // new
+                .addChild(regexRoleMapperParser) // new
                 .build();
     }
 }
