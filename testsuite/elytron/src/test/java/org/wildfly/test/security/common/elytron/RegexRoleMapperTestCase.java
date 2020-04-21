16d2f52df0ea828121e4be1d4f1e67cd6d4ebf62
dvilkola <dvilkola@redhat.com>
2020-04-21 12:21:57 +0200

    [WFCORE-4932] Reading of identity from security domain causes NPE

---
+++ testsuite/elytron/src/test/java/org/wildfly/test/security/common/elytron/RegexRoleMapperTestCase.java
@@ -0,0 +1,52 @@
+package org.wildfly.test.security.common.elytron;
+
+import org.jboss.as.test.integration.management.util.CLIWrapper;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.wildfly.core.testrunner.WildflyTestRunner;
+
+import static org.hamcrest.core.StringContains.containsString;
+
+@RunWith(WildflyTestRunner.class)
+public class RegexRoleMapperTestCase {
+    CLIWrapper cli;
+
+    @Before
+    public void setup() throws Exception {
+        cli = new CLIWrapper(true);
+        cli.sendLine("/subsystem=elytron/filesystem-realm=myFsRealm:add(path=my-fs-realm-users,relative-to=jboss.server.config.dir)");
+        cli.sendLine("/subsystem=elytron/filesystem-realm=myFsRealm:add-identity(identity=john)");
+        cli.sendLine("/subsystem=elytron/filesystem-realm=myFsRealm:add-identity-attribute(identity=john, name=Roles, value=[\"user\"])");
+        cli.sendLine("/subsystem=elytron/security-domain=mySD:add(realms=[{realm=myFsRealm}],default-realm=myFsRealm,permission-mapper=default-permission-mapper)");
+    }
+
+    @AfterClass
+    public void cleanup() throws Exception {
+        removeTestResources();
+        cli.close();
+    }
+
+    @Test
+    public void testReadIdentityFromSecurityDomain() {
+        boolean success = cli.sendLine("/subsystem=elytron/regex-role-mapper=rrm:add(pattern=\"guest\", replacement=\"\", keep-non-mapped=\"false\", replace-all=\"true\")", true);
+
+        Assert.assertFalse(success);
+        Assert.assertThat(cli.readOutput(), containsString("'' is an invalid value for parameter replacement. Values must have a minimum length of 1 characters\""));
+    }
+
+    @Test
+    public void testReadIdentityFromSecurityDomain2() {
+        boolean success = cli.sendLine("/subsystem=elytron/regex-role-mapper=rrm:add(pattern=\"guest\", replacement=\"\", keep-non-mapped=\"false\", replace-all=\"true\")", true);
+        Assert.assertFalse(success);
+        Assert.assertThat(cli.readOutput(), containsString("'' is an invalid value for parameter replacement. Values must have a minimum length of 1 characters\""));
+    }
+
+    private void removeTestResources() {
+        cli.sendLine("/subsystem=elytron/security-domain=mySD:remove");
+        cli.sendLine("/subsystem=elytron/filesystem-realm=myFsRealm:remove");
+        cli.sendLine("reload");
+    }
+}
